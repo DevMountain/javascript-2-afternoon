96254a01177e86751f237ffaef30bd08ec05bd54
Matthew Ecklund <mecklund@ldschurch.org>
2015-04-11 12:00:05 -0600

    halfway done with project

--- practice.js
+++ practice.js
@@ -4,6 +4,10 @@
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
   //Code here
+  var me = { 
+    name: "Matt", age: 29 
+  }
+  alert(me.name);
 
 
 
@@ -16,16 +20,28 @@
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
   //Code here
+  var favoriteThings = {
+    band: "Plan B",
+    food: "Protein",
+    person: "Jessica",
+    book: "The Lord of the Rings",
+    movie: "Superman",
+    holiday: "Christmas"
+  }
 
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
   //Code here
+  favoriteThings.car = "AC Cobra";
+  favoriteThings.brand = "Lodge Cast Iron";
 
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
 
   //Code here
+  favoriteThings.food = "Lettuce";
+  favoriteThings.book = "Anything but 50 Shades of Grey";
 
 
 
@@ -43,16 +59,30 @@ Using dot notation, add another key (or property) to your backPack object
 that is named color, with the value being the color of your backpack. */
 
   //Code here
+  var backPack = {};
+  var item = 'firstPocket';
+  backPack[item] = 'chapstick';
+
+  backPack.color = 'blue';
 
 //After you do the above, alert your entire backPack object.
 
   //Code here
+  alert(backPack);
+
+  //Alert just the values in this object do the following
+  var string = '';
+  for(key in backPack){
+      string = string + ' ' + backPack[key];
+  }
+  alert(string);
 
 /*You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. */
 
   //Code here
+  console.log(backPack);
 
 
 
@@ -65,10 +95,22 @@ Instead, console.log your whole backPack object and then check out the console. 
 //Create another 'me' object with the following properties name, age, height, gender, married, eyeColor, hairColor. Fill those properties in with the appropriate values.
 
   //Code Here
+  var me = {
+    name: 'Matt',
+    age: 29, 
+    height: '6\' 01\"', 
+    gender: 'Male',
+    married: true,
+    eyeColor: 'blue',
+    hairColor: 'brown'
+  }
 
 //Now, loop through your object and alert every value. *Tyler --> 24 --> 6'0 --> Male, etc etc
 
   //Code Here
+  for(key in me){
+    alert(me[key]);
+  };
 
 
 
@@ -81,10 +123,20 @@ Instead, console.log your whole backPack object and then check out the console. 
 //Create an Object called 'album' with 5 keys named different song titles that you make up, with the values being the length of each song.
 
   //Code Here
+  var album = {
+    stairwayToHeaven: 8,
+    blackDog: 5,
+    mistyMountainHop: 4.5,
+    whenTheLeveeBreaks: 7,
+    rockAndRoll: 3.75
+  }
 
 //Now, loop through your album object alerting every song title individually.
 
   //Code Here
+  for(key in album){
+    alert(key);
+  }
 
 
 
@@ -97,10 +149,22 @@ Instead, console.log your whole backPack object and then check out the console. 
 //Create an object called states that has 5 US states as properties with the values being their population (doesn't have to be accurate).
 
   //Code Here
+  var states = {
+    utah: 100000,
+    florida: 200000,
+    wisconsin: 125000,
+    idaho: 30000,
+    georgia: 175000
+  }
 
 //Now, loop through your states object and if the states population is greater than 30K, alert that state.
 
   //Code Here
+  for(key in states){
+    if(states[key] > 30000){
+      alert(key);
+    }
+  }
 
 
 
@@ -150,10 +214,13 @@ var user = {
 // name -> 'Tyler S. McGinnis', email -> 'tyler.mcginnis@devmounta.in'. Make that change.
 
   //Code Here
+  user.name = "Tyler S. McGinnis";
+  user.email = 'tyler.mcginnis@devmounta.in';
 
 //Now call the sayName method that's on the user object which will alert the users email
 
   //Code Here
+  user.sayName();
 
 
 
