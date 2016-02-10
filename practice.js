c81d52cbcdc1f3703297b9acf84ee215835bbee7
Joshua LeDuc <joshualeduc@gmail.com>
2016-02-10 10:43:30 -0700

    clarified makeCard and bind

--- practice.js
+++ practice.js
@@ -193,21 +193,19 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 
 
 
-// Create a function called MakeCard which takes in all the data it needs to make a Credit Card object and returns that object so that whenever you invoke MakeCard, you get a brand new credit card.
+// Create a function called MakeCard which takes in cardNumber, expirationDate, and securityCode to make a Credit Card object and returns that object so that whenever you invoke MakeCard, you get a brand new credit card.
 
   //Code Here
-  
-  
-  
+
+
+
 //NEXT PROBLEM
 
 
 
 /* As of this point you should have a MakePerson and a MakeCard function which returns you either a person or a credit card object.
    Now, create a bindCard function that takes in a person object as its first parameter and a creditcard object as its second parameter.
-   Have bindCard merge the two parameters together into a new object which contains all the properties from the person as well as the creditcard.
+   Have bindCard merge the two parameters together into a new object which contains all the properties from the person as well as the creditcard. You cannot use Object.assign.
 */
 
   //Code Here
-
-
