27b793de210d94f27001871f3ac027a7f1545b2a
jameslemire <webdev.jameslemire@gmail.com>
2018-01-04 12:45:11 -0700

    JL master - Create a similar feel across the JS projects.

--- practice.js
+++ practice.js
@@ -1,75 +1,71 @@
-//Once you complete a problem, open SpecRunner.html in Chrome and check to for failures.
-//Note: The default display will only show failed specs (tests), click "Spec List" to see all the conditions checked.
-//       * Passed specs will be in black, failed will be in red.
-//       * Refresh the page to rerun all specs, or click a spec to run it and its children (if any)
-
-
-//Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
-
-  //Code here
-
-
-
-
-
-
-//NEXT PROBLEM
-
-
-
-
-//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
-
-  //Code here
-
-
-
-
-
-
-
-//After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
-
-  //Code here
-
-
-
-
-
-//Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
-
-  //Code here
-
-
-
-
-
-//NEXT PROBLEM
-
-
-
 /* 
-Create an empty Object called backPack. Now, create a variable called 'item'
-and set it equal to the string 'firstPocket'. Using bracket notation,
-add a 'firstPocket' key (or property) to backPack, using 'item'.
-Set the value of that key to 'chapstick'.
+  Once you complete a problem, refresh ./SpecRunner.html in your browser and check to see if the problem's test(s) are passing.
+  Passed tests will be indicated by a green circle.
+  Failed tests will be indicated by a red X.
 
-Using dot notation, add another key (or property) to your backPack object
-that is named color, with the value being the color of your backpack. 
+  You can refresh the page at any time to re-run all the tests.
 */
 
-  //Code here
+////////// PROBLEM 1 //////////
+
+/*
+  Create an object called me. 
+  Give it a key of name with the value being your name, and another key of age with the value being your age. 
+  Then alert your name using dot notation.
+*/
+
+//Code here
 
 
 
+////////// PROBLEM 2 //////////
+
+/*
+  Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. 
+  Have the values to those keys be your favorite thing in that category.
+*/
+
+//Code here
 
 
 
-//After you do the above, alert your entire backPack object.
+/*
+  After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
+*/
 
-  //Code here
+//Code here
 
- 
+
+
+/*
+  Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
+*/
+
+//Code here
+
+
+
+////////// PROBLEM 3 //////////
+
+/* 
+  Create an empty Object called backPack. 
+  Now, create a variable called 'item' and set it equal to the string 'firstPocket'. 
+  Using bracket notation, add a 'firstPocket' key (or property) to backPack, using 'item'.
+  Set the value of that key to 'chapstick'.
+  Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack. 
+*/
+
+//Code here
+
+
+
+/*
+  After you do the above, alert your entire backPack object.
+*/
+
+//Code here
+
+
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -77,100 +73,80 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-  //Code here
+//Code here
 
 
 
+////////// PROBLEM 4 //////////
 
-
-//NEXT PROBLEM
-
-
-
-
+// Do not edit the code below.
 var user2 = {
-        name: 'Ty',
-        age: 24,
-        pwHash: 'U+Ldlngx2BYQk',
-        email: 'ty33@gmail.com',
-        birthday: '05/02/1990',
-        username: 'tylermcginnis33'
+  name: 'Ty',
+  age: 24,
+  pwHash: 'U+Ldlngx2BYQk',
+  email: 'ty33@gmail.com',
+  birthday: '05/02/1990',
+  username: 'tylermcginnis33'
 };
-//Let's say I, the user, decided to change my name and email address to the following
-// name -> 'Tyler S. McGinnis', email -> 'tyler.mcginnis@devmounta.in'. Make that change.
+// Do not edit the code above.
 
-  //Code Here
+/* 
+  Let's say I, the user, decided to change my name and email address to the following:
+  name -> 'Tyler S. McGinnis' and email -> 'tyler.mcginnis@devmounta.in'. 
+  Make that change without modifying the original object code above.
+*/
 
-
-
-
-
-// =============================================
-// =============================================
-// EXTRA PRACTICE PROBLEMS BELOW
-// =============================================
-// =============================================
+//Code Here
 
 
 
+////////// EXTRA PRACTICE PROBLEMS BELOW //////////
 
+////////// PROBLEM 5 //////////
 
+/*
+  Create an empty object called methodCollection.
+*/
 
-//NEXT PROBLEM
-
-
-//Create an empty object called methodCollection.
-
-  //Code Here
-
+//Code Here
 
 
 
 /*
-Now add two methods (functions that are properties on objects) to your methodCollection
-object. One called 'alertHello' which alerts 'hello' and another method called logHello
- which logs 'hello' to the console. 
- */
+  Now add two methods (functions that are properties on objects) to your methodCollection object. 
+  One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console. 
+*/
 
-  //Code Here
-
+//Code Here
 
 
 
+/*
+  Now call your alertHello and logHello methods.
+*/
 
-//Now call your alertHello and logHello methods.
-
-  //Code Here
-
-
-
+//Code Here
 
 
 
+////////// PROBLEM 6 //////////
 
+/* 
+  Create a function called makePerson which takes in name, birthday, ssn as its parameters. 
+  Return a new object with all of the information that you passed in.
+*/
 
-//NEXT PROBLEM
-
-
-
-// Create a function called makePerson which takes in name, birthday, ssn as its
-// parameters and returns a new object with all of the information that you passed in.
-
-  //Code Here
-
+//Code Here
 
 
 
+////////// PROBLEM 7 //////////
 
-  
+/*
+  Create a function called makeCard which takes in cardNumber, expirationDate, and securityCode to make a Credit Card object.
+  Return that object so that whenever you invoke makeCard, you get a brand new credit card.
+*/
 
-
-//NEXT PROBLEM
-
-
-
-// Create a function called makeCard which takes in cardNumber, expirationDate, and securityCode to make a Credit Card object and returns that object so that whenever you invoke makeCard, you get a brand new credit card.
-
-  //Code Here
+//Code Here
 
 
